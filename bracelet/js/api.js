7c7cafc6a4d9a9a55ca5b069ac8bef7802ddcd97
michael <108010812+MICKY1011@users.noreply.github.com>
2023-06-03 23:04:14 +0300

    second commit

--- bracelet/js/api.js
+++ bracelet/js/api.js
@@ -1,10 +1,9 @@
 // import { Splide } from "../../node_modules/@splidejs/splide";
 
-let slider = document.querySelector(".slider");
-let underSlider = document.querySelector(".under");
+let newSlider = document.querySelector(".newSlider");
+let underSlider = document.querySelector(".newThumbnail");
 let showShipping = document.querySelector(".show-shipping");
 let option = document.querySelector("#shipping");
-let splide = null;
 
 let products = [
   {
@@ -38,14 +37,14 @@ let shipping = [
 ];
 
 export default function displayImages(color) {
-  slider.innerHTML = "";
+  newSlider.innerHTML = "";
   underSlider.innerHTML = "";
   let select = products.find((product) => product.color === color);
   if (select) {
     select.images.forEach((element) => {
-      slider.innerHTML += `<li class="splide__slide"><img src=${element} alt=""/></li>`;
+      newSlider.innerHTML += `<li class="splide__slide"><img src=${element} alt=""/></li>`;
       underSlider.innerHTML += `<li class="thumbnail"><img src=${element} alt=""/></li>`;
-    });
+    });  
   }
 }
 
@@ -61,30 +60,30 @@ export function showShip(){
   }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  var splide = new Splide("#main-slide", {
-      pagination: false,
-      rewind: true,
-    });
-    var thumbnails = document.querySelectorAll(".thumbnail");
-    var current;
-    for (var i = 0; i < thumbnails.length; i++) {
-      initThumbnail(thumbnails[i], i);
-    }
-    function initThumbnail(thumbnail, index) {
-      thumbnail.addEventListener("click", function () {
-        splide.go(index);
+  document.addEventListener("DOMContentLoaded", function () {
+    var splide = new Splide("#main-slide", {
+        pagination: false,
+        rewind: true,
       });
-    }
-    splide.on("mounted move", function () {
-      var thumbnail = thumbnails[splide.index];
-      if (thumbnail) {
-        if (current) {
-          current.classList.remove("is-active");
-        }
-        thumbnail.classList.add("is-active");
-        current = thumbnail;
+      var thumbnails = document.querySelectorAll(".thumbnail");
+      var current;
+      for (var i = 0; i < thumbnails.length; i++) {
+        initThumbnail(thumbnails[i], i);
       }
+      function initThumbnail(thumbnail, index) {
+        thumbnail.addEventListener("click", function () {
+          splide.go(index);
+        });
+      }
+      splide.on("mounted move", function () {
+        var thumbnail = thumbnails[splide.index];
+        if (thumbnail) {
+          if (current) {
+            current.classList.remove("is-active");
+          }
+          thumbnail.classList.add("is-active");
+          current = thumbnail;
+        }
+      });
+      splide.mount();
     });
-    splide.mount();
-  });
--- bracelet/js/pay.js
+++ bracelet/js/pay.js
@@ -1,5 +1,8 @@
 import displayImages , {showShip} from "./api.js";
 
+let slider = document.querySelector(".slider");
+let under = document.querySelector(".under");
+
 let colorSelector = document.querySelectorAll(".color");
 let option = document.querySelector("#shipping");
 
@@ -9,10 +12,16 @@ option.addEventListener("change", function () {
 
 colorSelector[0].addEventListener("click", () => {
   displayImages("black");
+  slider.innerHTML = ""
+  under.innerHTML = ""
 });
 colorSelector[1].addEventListener("click", () => {
   displayImages("white");
+  slider.innerHTML = ""
+  under.innerHTML = ""
 });
 colorSelector[2].addEventListener("click", () => {
   displayImages("brown");
+  slider.innerHTML = ""
+  under.innerHTML = ""
 });
